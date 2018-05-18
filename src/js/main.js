eb32a0264a46d681ad44c0ebf25e74c0790e480c
Nataly <ncgarzon88@gmail.com>
2018-05-18 13:02:18 -0400

    in class updates

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,13 @@
 		document.body.addEventListener('keyup', function () {
 			// check to see if the string includes . and @
 			if (email.value.includes('.') && email.value.includes('@')) {
-				email.style.border = "5px solid green";
+				// email.style.border = "5px solid green";
+				emailInput.classList.remove('fail');
+				emailInput.classList.add('pass');
 			} else {
-				email.style.border = "5px solid red"; 
+				// email.style.border = "5px solid red"; 
+				emailInput.classList.remove('pass');
+				emailInput.classList.add('fail');
 			}
 		})
 // 	}
